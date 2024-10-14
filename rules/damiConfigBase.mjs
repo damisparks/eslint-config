ef187c37d571e5509331228d28b6cbdbf27702e2
Dami Sparks <damisparks@outlook.com>
2024-10-14 16:49:47 +0200

    update rules in `dsConfigVue`

--- rules/damiConfigBase.mjs
+++ rules/damiConfigBase.mjs
@@ -2,11 +2,10 @@ const dsConfigBase = [
   {
     name: 'damisparks/base',
     rules: {
-      // TODO Review these rules
       'antfu/top-level-function': 'off',
-      'curly': ['error', 'all'],
       'node/prefer-global/process': 'off',
       'style/function-call-spacing': ['error', 'never'],
+      'style/no-tabs': ['error', { allowIndentationTabs: true }],
     },
   },
 ]
