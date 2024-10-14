ebd77c39b94816708bfbf8451cbf190d8fd6951a
Dami Sparks <damisparks@outlook.com>
2024-10-14 11:16:23 +0200

    update `vue/max-attributes-per-line` rule

--- rules/dsConfigVue.mjs
+++ rules/dsConfigVue.mjs
@@ -4,7 +4,10 @@ const dsConfigVue = [
     files: ['**/*.vue'],
     rules: {
       'vue/block-order': ['error', { order: ['script', 'template', 'style'] }],
-      'vue/max-attributes-per-line': ['error', { singleline: 3, multiline: { max: 1, allowFirstLine: false } }],
+      'vue/max-attributes-per-line': ['error', {
+        singleline: { max: 3 },
+        multiline: { max: 1 },
+      }],
       'vue/prop-name-casing': ['error', 'camelCase'],
       'vue/multi-word-component-names': 'warn',
     },
